3bbfbb171754ab78f16c323817e5917b8538e073
BT_3N3_13 <email@emailk>
2025-03-03 10:22:52 -0500

    extra cxoiolio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Imma rectangle");
+        Rectangles[] rectanglesAMillion = new Rectangles[1000000];
+        for (int i = 0; i < 100000; i++)
+        {
+            rectanglesAMillion[i] = new Rectangles();
+        }
+        for (int i = 0; i < 100000; i++)
+        {
+            System.out.println(rectanglesAMillion[i]);
+        }
     }
 }
