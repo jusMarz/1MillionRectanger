3906864efafb79d8655e41d4c301508b253eb80a
BT_3N3_13 <email@emailk>
2025-03-03 09:55:31 -0500

    rectamngle class made

---
+++ src/Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle
+{
+    private int cornerX;
+    private int cornerY;
+    private int length;
+    private int height;
+
+    public Rectangle(int x, int y, int len, int height)
+    {
+        cornerX = x;
+        cornerY = y;
+        length = len;
+        this.height = height;
+    }
+
+    public Rectangle()
+    {
+        cornerX = Math.;
+        cornerY = y;
+        length = len;
+        this.height = height;
+    }
+}
