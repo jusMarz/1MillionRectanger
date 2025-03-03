e9fcdb79031643a91e17f7bffa98e66c2ba395ce
BT_3N3_14 <f@f>
2025-03-03 10:10:55 -0500

    working on overlap boolean

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Imma rectangle");
+        Rectangle initialRectangle = new Rectangle(10, 15, 20, 10);
+
     }
 }
